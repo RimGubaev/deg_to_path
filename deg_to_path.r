68e753b8f519a02128ce82c02236cf0b2a349b79
Rim <rimgubaev@gmail.com>
2019-02-13 12:57:50 +0300

    Update deg_to_path.r

--- deg_to_path.r
+++ deg_to_path.r
@@ -1,54 +1,67 @@
 # Устанавливаем рабочую директорию
+# Set the working directory
 setwd("~/your/working/directory/")
 
 # Загружаем библиотеку "plyr"
+# Load the "plyr" library
 library("plyr")
 
 ############################################
 # 1 Выявляем пути A.bispodus, достоверно насыщенные активируемыми/репрессируемыми генами L. edodes
+# 1 Find A.bispodus pathways significantly enriched by activated/repressedgenes of L. edodes
 ############################################
 
 # Загружаем таблицу c транскриптами L.edodes (Led), распределенными по функциональным категориям KEGG для A.bishorus (Abi)
+# Upload table with L.edodes (Led) transcripts, combined into KEGG functional categories of A.bishorus (Abi)
 Led_path <- read.csv(file = "Led_Abi_pathways.csv", header = T)
 path_names <- read.csv(file = "abi_pathway_name", sep = "\t", header = F)
 path_names <- data.frame(path_num=path_names$V1, path_name=path_names$V2)
 
 # Распределяем дифференциально экспрессирующиеся гены по метаболическим путям
+# Classify DEGs according to metabolic pathways 
 diff_exp <- read.csv(file = "gene_exp.diff", sep = "\t")
 diff_exp <- diff_exp[ ,c(4,11,13,14,15)]
 diff_exp_path <- merge(x = diff_exp, y = Led_path, by.x = "locus", by.y = "assembled_transcript")
 diff_exp_path <- subset(diff_exp_path, diff_exp_path$p_value < 0.05)
 
 # Из таблицы с аннотированными ДЭГ извлекаем достоверно активируемые
+# Extracting significantly activated DEGs from the table
 diff_exp_up <- subset(diff_exp_path, diff_exp_path$log2.fold_change. > 0)
 
 # Из таблицы с аннотированными ДЭГ извлекаем достоверно репрессируемые
+# Extracting significantly repressed DEGs from the table
 diff_exp_down <- subset(diff_exp_path, diff_exp_path$log2.fold_change. < 0)
 
 # Подсчитываем общее количество генов для каждого пути
+# Count the total ammount of genes for each pathway
 pathway_table <- as.data.frame(count(Led_path$path_num))
 
 # Подсчитываем количество активируемых генов для каждого пути
+# Count the total ammount of activated genes for each pathway
 pathway_table_up <- as.data.frame(count(diff_exp_up$path_num))
 colnames(pathway_table_up)[2] <- "up_in_pathway"
 
 # Подсчитываем количество репрессируемых генов для каждого пути
+# Count the total ammount of repressed genes for each pathway
 pathway_table_down <- as.data.frame(count(diff_exp_down$path_num))
 colnames(pathway_table_down)[2] <- "down_in_pathway"
 
 # Объединяем посчитанные количество активируемых/репрессируемых генов для всех путей
+# Merge counted amount of activated/repressed genes for all pathways
 UP_DOWN_pathways <- Reduce(function(x, y) merge(x, y, all=TRUE), list(pathway_table, pathway_table_up, pathway_table_down))
 UP_DOWN_pathways[is.na(UP_DOWN_pathways )] <- 0 
 colnames(UP_DOWN_pathways)[1] <- "path_num"
 colnames(UP_DOWN_pathways)[2] <- "genes_in_path"
 
 
-# подсчет количества активируемых и репрессируемых генов, всех генов, вошедших в пути
+# Подсчет количества активируемых и репрессируемых генов, всех генов, вошедших в пути
+# Count the aounts of activated and repressed genes, all genes within pathways  
 up_DEG <- nrow(diff_exp_up)
 down_DEG <- nrow(diff_exp_down)
 all_Genes_in_path <- nrow(Led_path)
 
 #Тест Фишера для активируемых генов
+# Fisher test for activated genes
 UP_pathways <- UP_DOWN_pathways[ ,c(-4)]
 UP_pathways$up_not_in_pathway <- up_DEG - UP_pathways$up_in_pathway
 UP_pathways$no_up_in_pathway <- UP_pathways$genes_in_path - UP_pathways$up_in_pathway
@@ -60,6 +73,7 @@ for(i in 1:n) {
   dfxm <- matrix(dat, nrow = 2)
   UP_pathways$p.value[i] <- fisher.test(dfxm, alternative = "greater")$p.value
   # Подсчет соотношения шансов для каждого пути
+  # Count the odds ration for each pathway
   UP_pathways$odds.ratio[i] <- fisher.test(dfxm, alternative = "greater")$estimate
   print (UP_pathways$p.value[i])
 }
@@ -67,9 +81,11 @@ for(i in 1:n) {
 # Делаем поправку на множественное тестирование (False Discovery Rate)
 UP_pathways$q.value <- (p.adjust(UP_pathways$p.value, method = "fdr"))
 # Присваеваем путям названия
+# Adding a names to the pathway                  
 UP_pathways <- merge(x = UP_pathways[,-c(4:6)], y = path_names, by = "path_num")
 
-#Тест Фишера для репрессируемых генов 
+# Тест Фишера для репрессируемых генов
+# Fisher test for activated genes
 DOWN_pathways <- UP_DOWN_pathways[ ,-3]
 DOWN_pathways$down_not_in_pathway <- down_DEG - DOWN_pathways$down_in_pathway
 DOWN_pathways$no_down_in_pathway <- DOWN_pathways$genes_in_path - DOWN_pathways$down_in_pathway
@@ -82,13 +98,16 @@ for(i in 1:n) {
   dfxm <- matrix(dat, nrow = 2)
   DOWN_pathways$p.value[i] <- fisher.test(dfxm, alternative = "greater")$p.value
   # Подсчет соотношения шансов для каждого пути
+  # Count the odds ration for each pathway
   DOWN_pathways$odds.ratio[i] <- fisher.test(dfxm, alternative = "greater")$estimate
   print (DOWN_pathways$p.value[i])
 }
 
 # Делаем поправку на множественное тестирование (False Discovery Rate)
+# Make an FDR correction
 DOWN_pathways$q.value <- (p.adjust(DOWN_pathways$p.value, method = "fdr"))
 # Присваеваем путям названия
+# Adding a names to the pathways
 DOWN_pathways <- merge(x = DOWN_pathways[,-c(4:6)], y = path_names, by = "path_num")
 
 # Записываем полученные результаты в таблицу 
@@ -142,6 +161,7 @@ down_DEG <- nrow(diff_exp_down)
 all_Genes_in_path <- nrow(Led_KEGG_path)
 
 #Тест Фишера для активируемых генов
+# Fisher test for activated genes
 UP_pathways <- UP_DOWN_pathways[ ,c(-4)]
 UP_pathways$up_not_in_pathway <- up_DEG - UP_pathways$up_in_pathway
 UP_pathways$no_up_in_pathway <- UP_pathways$genes_in_path - UP_pathways$up_in_pathway
@@ -162,7 +182,8 @@ UP_pathways$q.value <- (p.adjust(UP_pathways$p.value, method = "fdr"))
 # Присваеваем путям названия
 UP_pathways <- merge(x = UP_pathways[,-c(4:6)], y = path_names, by = "path_num")
 
-#Тест Фишера для репрессируемых генов 
+# Тест Фишера для репрессируемых генов 
+# Fisher test for repressed genes
 DOWN_pathways <- UP_DOWN_pathways[ ,-3]
 DOWN_pathways$down_not_in_pathway <- down_DEG - DOWN_pathways$down_in_pathway
 DOWN_pathways$no_down_in_pathway <- DOWN_pathways$genes_in_path - DOWN_pathways$down_in_pathway
